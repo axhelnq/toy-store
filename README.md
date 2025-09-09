77035d30a3b2d9679d362a272a1794d12eb99ff4
axhelnq <axhelnq@gmail.com>
2025-09-09 21:55:36 +0300

    feat: navigation JS + responsive tweaks + style polish

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 
 **ToyStore** – a simple and stylish landing page for an online children's toy store.  
 Made with clean **HTML** and **CSS**.
-
+Figma design - <a href="https://www.figma.com/design/cCXY8L3KImuisO8eJT1s0d/ToyStore?node-id=0-1&p=f&t=HWNIbO8j4SUqThmQ-0">click</a>
 <br/>
 
 ---
@@ -21,7 +21,10 @@ Made with clean **HTML** and **CSS**.
       <img src="https://upload.wikimedia.org/wikipedia/commons/6/62/CSS3_logo.svg" width="80"/>
       <br>CSS3
     </td>
-
+    <td align="center">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/3/33/Figma-logo.svg" width="53"/>
+      <br>Figma
+    </td>
 </table>
 
 ---
---
+++ main.js
@@ -0,0 +1,49 @@
+const toyTypeEl = document.querySelector('.toy-type');
+const scrollToToyTypeEl = () => {
+  window.scrollTo({
+    top: toyTypeEl.offsetTop,
+    behavior: 'smooth'
+  })
+}
+
+const stuffedEl = document.querySelector('.stuffed-catalogue');
+const scrollToStuffedEl = () => {
+  window.scrollTo({
+    top: stuffedEl.offsetTop,
+    behavior: 'smooth'
+  })
+}
+
+
+
+const woodenEl = document.querySelector('.wooden-catalogue');
+const scrollToWoodenEl = () => {
+  window.scrollTo({
+    top: woodenEl.offsetTop,
+    behavior: 'smooth'
+  });
+}
+
+const topBarEl = document.querySelector('.top-bar');
+const scrollToTop = () => {
+  window.scrollTo({
+    top: topBarEl.offsetTop,
+    behavior: 'smooth'
+  })
+}
+
+const instagramEl = document.querySelector('.instagram');
+const scrollToInstagramEl = () => {
+  window.scrollTo({
+    top: instagramEl.offsetTop,
+    behavior: 'smooth'
+  })
+}
+
+const trialEl = document.querySelector('.trial');
+const scrollToTrialEl = () => {
+  window.scrollTo({
+    top: trialEl.offsetTop,
+    behavior: 'smooth'
+  })
+}
